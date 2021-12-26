6bd260d0aeb20bbbf4125c7611d8dbfff659c9d3
Zane Milakovic <zane.milakovic@icloud.com>
2021-12-26 13:17:55 -0500

    adds builds to vercel.json

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 amazon-linux-extras install golang1.11
 echo "Amazon Extras Ran"
-GOARCH=amd64 GOOS=linux go build output/server/pages/main.go -ldflags="-s -w"
+# GOARCH=amd64 GOOS=linux go build main.go -ldflags="-s -w"
 mv output .output
