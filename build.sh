ba8d6f5dbb5da7398f844b2c020c5152834b041a
Zane Milakovic <zane.milakovic@icloud.com>
2021-12-26 10:59:25 -0500

    update build

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-go build output/server/pages/timeBkup.go
+amazon-linux-extras install golang1.17
+GOARCH=amd64 GOOS=linux go build output/server/pages/main.go -ldflags="-s -w"
 mv output .output
