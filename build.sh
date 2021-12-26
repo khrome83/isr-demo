c95afb54064fe3d03d75f5dd81582c0f9bccada8
Zane Milakovic <zane.milakovic@icloud.com>
2021-12-26 11:00:58 -0500

    changes build version

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-amazon-linux-extras install golang1.17
+amazon-linux-extras install golang1.11
 GOARCH=amd64 GOOS=linux go build output/server/pages/main.go -ldflags="-s -w"
 mv output .output
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/khrome83/isr-demo
 
-go 1.17
+go 1.11
